02f4a9e290d7a47c6958d16d6bc2829ec6a52ac5
Mac User <BradleyMitchellis@gmail.com>
2017-09-08 00:16:48 -0400

    Preparation for content writer

--- wp-content/themes/lighthouse/template-parts/content-writer.php
+++ wp-content/themes/lighthouse/template-parts/content-writer.php
@@ -1,8 +1,18 @@
 <?php if(have_rows('writer_block', 'option')) : ?>
   <?php while(have_rows('writer_block', 'option')) : the_row(); ?>
+    <?php
+      $profile_image = get_sub_field('photo', 'option');
+      $name = get_sub_field('name', 'option');
+      $bio = get_sub_field('description', 'option');
+    ?>
 
     <div class="writers-block">
-      Hey yall
+      <div class="img-wrap img-wrap-small">
+          <img src="<?php echo $profile_image['sizes']['resource-thumb'] ?>" alt="<?php echo $item_img["alt"]; ?>" />
+      </div>
+      <p><?php echo $name ?></p>
+      <div class="tag tag--mentor">MENTOR</div>
+      <div class="desc"><?php echo $bio ?></div>
     </div>
 
   <?php endwhile; ?>
