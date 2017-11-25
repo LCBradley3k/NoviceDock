eb37c784acdeb462f3abd5c714593aed6da98773
Mac User <BradleyMitchellis@gmail.com>
2017-11-25 14:00:00 -0500

    Fix open in new tab, both tabs redirecting to URL

--- wp-content/themes/lighthouse/header.php
+++ wp-content/themes/lighthouse/header.php
@@ -42,8 +42,7 @@
   */
   var trackOutboundLink = function(url) {
      ga('send', 'event', 'outbound', 'click', url, {
-       'transport': 'beacon',
-       'hitCallback': function(){document.location = url;}
+       'transport': 'beacon';}
      });
   }
 
